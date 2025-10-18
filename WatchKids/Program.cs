f247553721872bf594f05fd5a17747b9735ff17a
Ahmed525-12 <ahmedsayed5250057@gmail.com>
2025-10-18 21:50:52 +0300

    make swagger public

--- WatchKids/Program.cs
+++ WatchKids/Program.cs
@@ -31,11 +31,8 @@ namespace WatchKids
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             var app = builder.Build();
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseAuthorization();
